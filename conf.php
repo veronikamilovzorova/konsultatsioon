8ae54807e0b07ffe224caa3035cb23ddc1d021ee
Opilane TTHK <opilane@tthk.ee>
2024-05-06 09:49:05 +0300

    ülesanne 2 uuendasin background värvid kõikides css vailides

--- conf.php
+++ conf.php
@@ -1,7 +1,8 @@
 <?php
-$kasutaja='opilane';
-$serverinimi='localhost';
-$parool='123';
-$andmebaas='veronika';
-$yhendus=new mysqli($serverinimi, $kasutaja, $parool, $andmebaas);
+$kasutaja='d123175_luca';
+$sererinimi='d123175.mysql.zonevs.eu';
+$parool='pentagonanetu27';
+$andmebaas='d123175_andmebaas';
+$yhendus=new mysqli($sererinimi,$kasutaja,$parool,$andmebaas);
 $yhendus->set_charset('UTF8');
+?>
